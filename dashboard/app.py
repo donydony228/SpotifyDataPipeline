637019aa613a2e67c6099274309e201ca0cc8a29
donydony228 <donydony228@gmail.com>
2026-01-02 14:30:08 -0500

    Update reqiorement

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,14 +7,22 @@ st.set_page_config(
 )
 
 # Navigation setup
-pg = st.navigation([
+pg = st.navigation({
+    "Analytics": [
     st.Page("main_page.py", title="Home"),
     st.Page("track.py", title="Track"),
     st.Page("artist.py", title="Artist"),
     st.Page("album.py", title="Album"),
+    ],
+    "Data Governance": [
     st.Page("data_quality.py", title="Data Quality"),
+    # st.Page("data_dictionary.py", title="Data Dictionary"),
+    # st.Page("data_dictionary_admin.py", title="Data Dictionary Admin"),
+    ],
+    "About": [  
     st.Page("about.py", title="About"),
-])
+    ],
+})
 
 # Run the selected page
 pg.run()
